5c381f01deabd97cac3353e617815260a295c8ba
zengbin996 <victorybin@foxmail.com>
2022-08-29 10:59:34 +0800

    自动获取焦点

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,31 @@
-import React, { useRef, useState } from 'react'
-import { Button, Input, } from 'antd';
+import React, { useRef, useState } from 'react';
+import { Button, Input } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
-import { Translate, Avatar, DarkMode, WavesLeft, Github } from '@icon-park/react'
+import {
+  Translate,
+  Avatar,
+  DarkMode,
+  WavesLeft,
+  Github,
+} from '@icon-park/react';
 
 export default function Header() {
-
-  const [search, setSearch] = useState(false)
-  const searchInputRef: any = useRef(null)
+  const [search, setSearch] = useState(false);
   const searchHandled = function () {
-    setSearch(!search)
-    searchInputRef.focus()
-  }
+    setSearch(!search);
+  };
+
+  const inputBlurHandle = function () {
+    setSearch(!search);
+  };
 
   return (
-    <div className='h-16 border-b border-gray-300'>
-      <div className='container mx-auto h-full flex justify-between items-center'>
-        <div className='flex'>
-          <a href="/" className='mr-4 flex items-center gap-1'>
+    <div className="h-16 border-b border-gray-300">
+      <div className="container mx-auto h-full flex justify-between items-center">
+        <div className="flex">
+          <a href="/" className="mr-4 flex items-center gap-1">
             <WavesLeft theme="outline" size="24" fill="#D43030" />
-            <span className='text-xl text-gray-800'>Music</span>
+            <span className="text-xl text-gray-800">Music</span>
           </a>
 
           <Button type="text">推荐</Button>
@@ -26,21 +33,50 @@ export default function Header() {
           <Button type="text">歌单</Button>
           <Button type="text">歌手</Button>
 
-
-
-          {!search && <Button shape="circle" icon={<SearchOutlined />} onClick={searchHandled} />}
-          {search && <Input placeholder="开始搜索" ref={searchInputRef} size="small" />}
+          {!search && (
+            <Button
+              shape="circle"
+              icon={<SearchOutlined />}
+              onClick={searchHandled}
+            />
+          )}
+          {search && (
+            <Input
+              placeholder="开始搜索"
+              ref={function (input) {
+                if (input != null) {
+                  input.focus();
+                }
+              }}
+              size="small"
+              onBlur={inputBlurHandle}
+            />
+          )}
         </div>
 
-        <div className='flex gap-4'>
-          <DarkMode theme="outline" size="24" fill="#333" className='cursor-pointer' />
-          <Translate theme="outline" size="24" fill="#333" className='cursor-pointer' />
-          <Avatar theme="outline" size="24" fill="#333" className='cursor-pointer' />
-          <div className='border-r border-gray-200'></div>
+        <div className="flex gap-4">
+          <DarkMode
+            theme="outline"
+            size="24"
+            fill="#333"
+            className="cursor-pointer"
+          />
+          <Translate
+            theme="outline"
+            size="24"
+            fill="#333"
+            className="cursor-pointer"
+          />
+          <Avatar
+            theme="outline"
+            size="24"
+            fill="#333"
+            className="cursor-pointer"
+          />
+          <div className="border-r border-gray-200"></div>
           <Github theme="outline" size="24" fill="#333" />
         </div>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
-
